4b69f859201ac24f208e7d47aed06025d8621a38
User <User@10.3.227.25>
2024-05-09 13:24:44 +0800

    test

---
+++ homepage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>INTI Coffee | Login</title>
+        
+        <!-- Bootstrap 5 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <header class="site-header position-absolute">
+            <h1>INTI Coffee</h1>
+        </header>
+        <div class="col-md-4 mx-auto py-5">
+            
+        </div>
+    </body>
+</html>
