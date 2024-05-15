d3164984b8659aa166b8225e4bf1e2b5708a0cd4
User <User@10.3.227.25>
2024-05-15 15:35:55 +0800

    test

--- homepage.php
+++ homepage.php
@@ -13,11 +13,31 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
-        <header class="site-header position-absolute">
+        <header class="site-header text-center bg-light pt-3">
             <h1>INTI Coffee</h1>
+            
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="collapse navbar-collapse justify-content-center">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active h4"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item h4"><a class="nav-link" href="#">second</a></li>
+                        <li class="nav-item h4"><a class="nav-link" href="#">third</a></li>
+                        <li class="nav-item h4">&nbsp;&nbsp;</li>
+                    </ul>
+                    <form class="form-inline d-flex">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
         </header>
+        
         <div class="col-md-4 mx-auto py-5">
             
         </div>
+        
+        <footer>
+            
+        </footer>
     </body>
 </html>
